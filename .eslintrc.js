3bfbaf96d55440cdbdc22d1e77e9696892c153bd
codearoni <jake88brown@gmail.com>
2018-04-01 23:35:27 -0500

    Added eslint holla!

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 6,
+    },
+    rules: {
+        'semi': 2,
+        'no-trailing-spaces': 2,
+        'no-mixed-spaces-and-tabs': 2,
+        'prefer-const': 2,
+        'no-var': 2
+    }
+}
